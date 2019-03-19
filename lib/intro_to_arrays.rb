3125bc7d9fb18a071b4401cd53965af28a21430a
Joanna Yuhas <joannayuhas@gmail.com>
2019-03-19 01:45:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -17,3 +17,8 @@ def third_element(array)
    taylor_swift = ["Welcome to New York", "Blank Space", "Style","Out of the Woods"]
    taylor_swift[2]
  end
+ 
+ def last_element(array)
+    taylor_swift = ["Welcome to New York", "Blank Space", "Style","Out of the Woods"]
+    taylor_swift[-1]
+end
