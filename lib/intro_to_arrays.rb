ee48a472bc841bea2786c0b9eb0a8e569fbc8434
Joanna Yuhas <joannayuhas@gmail.com>
2019-03-19 01:42:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,4 +6,7 @@ def array_with_two_elements
   array = [1,2]
 end
 
-def first_element()
+def first_element(array)
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style","Out of the Woods"]
+  taylor_swift[0]
+end
